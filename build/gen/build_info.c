60989818767579d2e29fcde814b6f7a318f13438
Alex Haivoronsky <ogajvoronsky@gmail.com>
2018-02-19 13:10:40 +0200

    first commit

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180218-233821/???";
+const char *build_timestamp = "2018-02-18T23:38:21Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20180218-233821/???";
+const char *mg_build_timestamp = "2018-02-18T23:38:21Z";
+const char *mg_build_version = "2018021823";
